feffe83acd804a12229ae1589af9e1e10a0bb573
EthanPayne1 <payne.ethan6@yahoo.com>
2025-02-11 16:07:31 -0600

    resolved build errors

--- Main.c
+++ Main.c
@@ -6,7 +6,6 @@
 // PGresult encapsulates the result of a single SQL command (Query) -- A Query string given to PQsendQuery can conain multiple commands and thus return multiple PGresult objects)
 // PGfinish closes the current connection and frees the PG conn data structure
 void check_query(PGresult *results, PGconn *connection) {
-  // Error: Incompatible integer to pointer conversion passing 'int' to type const PGresults
   if (PQresultStatus(results) != PGRES_TUPLES_OK) {
     fprintf(stderr, "Line 7: Failed to Query: %s\n", PQerrorMessage(connection));
     PQclear(results);
@@ -20,11 +19,12 @@ void query_database() {
   PGconn* connection = PQconnectdb(connection_info);
   PGresult* res;
   if (PQstatus(connection) != CONNECTION_OK) {
-    printf(stderr, "Line 24: Failed to create connection: %s\n", PQerrorMessage(connection));
+    fprintf(stderr, "Failed to create connection: %s\n", PQerrorMessage(connection));
+    PQfinish(connection);
     exit(1);
   }
 
-  // Would rather not hardcode these vaules
+  // Would rather not hardcode these values
   double lon = -122.42;
   double lat = 37.77;
 
@@ -32,24 +32,24 @@ void query_database() {
   snprintf(query, sizeof(query),
            "SELECT ST_VALUE(rast, ST_SRID(ST_Point(%f, %f), 4326))"
            "FROM dted_table "
-           "WHERE ST_Intersects(rast, ST_SetSRID(ST_Point(%f, %f) -4326));",
+           "WHERE ST_Intersects(rast, ST_SetSRID(ST_Point(%f, %f), 4326));",
            lon, lat, lon, lat);
  
-  PGresult PGexec();
+  res = PQexec(connection, query);
   check_query(res, connection);
 
   if (PQntuples(res) > 0) {
     printf("Elevation: %s meters\n", PQgetvalue(res, 0, 0));
-  }
+  } 
   else {
-    printf("No elevation data found at this location. \n"); // Not possible but need to check
-    exit(1);
+    printf("No elevation data found at this location.\n");
   }
-  
+
+  PQclear(res);
+  PQfinish(connection);
 }
 
 int main() {  
   query_database();
-
   return 0;
 }
