a787cb47f674a062d9d8991de95d40cb00934f34
Ethan P <99617707+EthanPayne1@users.noreply.github.com>
2025-02-11 16:09:02 -0600

    Bug fix (#1)
    * new updates
    
    * continuing build
    
    * cleaned build process
    
    * resolved build errors

--- Main.c
+++ Main.c
@@ -1,14 +1,12 @@
 #include <stdatomic.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "/opt/homebrew/Cellar/libpq/17.2/include/libpq-fe.h"
-
+#include "/opt/homebrew/include/libpq-fe.h"
 // PGconn encapsulated a connection to the backend 
 // PGresult encapsulates the result of a single SQL command (Query) -- A Query string given to PQsendQuery can conain multiple commands and thus return multiple PGresult objects)
 // PGfinish closes the current connection and frees the PG conn data structure
 void check_query(PGresult *results, PGconn *connection) {
-  // Error: Incompatible integer to pointer conversion passing 'int' to type const PGresults
-  if (PQresultStatus((results) != PGRES_TUPLES_OK)) {
+  if (PQresultStatus(results) != PGRES_TUPLES_OK) {
     fprintf(stderr, "Line 7: Failed to Query: %s\n", PQerrorMessage(connection));
     PQclear(results);
     PQfinish(connection);
@@ -16,36 +14,42 @@ void check_query(PGresult *results, PGconn *connection) {
   }
 }
 
-int main() {  
-  const char *connection_info = "dbname=dted_db user=systemUser password=systemPassword host=localhost";
-  PGconn *connection = PQconnectdb(connection_info);
-  
+void query_database() {
+  const char *connection_info = "dbname=dted_db user=ethan host=localhost";
+  PGconn* connection = PQconnectdb(connection_info);
+  PGresult* res;
   if (PQstatus(connection) != CONNECTION_OK) {
-    // Print error
+    fprintf(stderr, "Failed to create connection: %s\n", PQerrorMessage(connection));
+    PQfinish(connection);
+    exit(1);
   }
+
+  // Would rather not hardcode these values
   double lon = -122.42;
   double lat = 37.77;
 
   char query[512];
   snprintf(query, sizeof(query),
-           "SELECT ST_VALUE(rast, ST_SRID(ST_Point(%f, $f), 4326))"
+           "SELECT ST_VALUE(rast, ST_SRID(ST_Point(%f, %f), 4326))"
            "FROM dted_table "
-           "WHERE ST_Intersects(rast, ST_SetSRID(ST_Point(%f, %f), -4326));",
+           "WHERE ST_Intersects(rast, ST_SetSRID(ST_Point(%f, %f), 4326));",
            lon, lat, lon, lat);
+ 
+  res = PQexec(connection, query);
+  check_query(res, connection);
 
-  // Error: Incompatible integer to pointer conversion passing 'int' to type const PGresults * 
-  PGresult *results = PGexec(connection, query);
-  check_query(results, connection);
-
-  if (PQntuples(results) > 0) {
-    printf("Elevation: %s meters\n", PGgetvalue(results, 0, 0));
-  }
+  if (PQntuples(res) > 0) {
+    printf("Elevation: %s meters\n", PQgetvalue(res, 0, 0));
+  } 
   else {
-    printf("No elevation data found at this location. \n"); // Not possible but checking
+    printf("No elevation data found at this location.\n");
   }
 
-  // Error: call to undeclared function
-  PGclear(results);
+  PQclear(res);
   PQfinish(connection);
+}
+
+int main() {  
+  query_database();
   return 0;
 }
