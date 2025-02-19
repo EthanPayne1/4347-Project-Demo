d72a49fc9ace0799a74fa1f1976fcbdbdcb74303
Ethan P <99617707+EthanPayne1@users.noreply.github.com>
2025-02-18 21:44:25 -0600

    Updated working build (#2)
    * Working build
    
    * Successfully querried from multiple dted files

--- Main.c
+++ Main.c
@@ -14,8 +14,8 @@ void check_query(PGresult *results, PGconn *connection) {
   }
 }
 
-void query_database() {
-  const char *connection_info = "dbname=dted_db user=ethan host=localhost";
+void query_database(double lon, double lat) {
+  const char *connection_info = "dbname=dted_database user=ethan host=localhost";
   PGconn* connection = PQconnectdb(connection_info);
   PGresult* res;
   if (PQstatus(connection) != CONNECTION_OK) {
@@ -24,13 +24,9 @@ void query_database() {
     exit(1);
   }
 
-  // Would rather not hardcode these values
-  double lon = -122.42;
-  double lat = 37.77;
-
   char query[512];
   snprintf(query, sizeof(query),
-           "SELECT ST_VALUE(rast, ST_SRID(ST_Point(%f, %f), 4326))"
+           "SELECT ST_VALUE(rast, ST_SetSRID(ST_Point(%f, %f), 4326))"
            "FROM dted_table "
            "WHERE ST_Intersects(rast, ST_SetSRID(ST_Point(%f, %f), 4326));",
            lon, lat, lon, lat);
@@ -49,7 +45,35 @@ void query_database() {
   PQfinish(connection);
 }
 
-int main() {  
-  query_database();
+int main() {
+  // Would rather not hardcode these values
+  
+  // n48_w114: Highest point = 3171m @ lon/lat=(-113.848, 48.9249)
+  //double lon = -113;
+  //double lat = 48;
+  
+  // n48_115: Highest point = 3060m @ lon/lat=(-114.172,48.944)
+  double lon_115 = -144.172;
+  double lat_115 = 48.944;
+  
+  // n48_116: Highest point = 2629m @ lon/lat=(-115.689,48.2229)
+  //double lon = -115.689;
+  //double lat = 48.2229;
+  
+  // n48_117: Highest point = 2349m @ lon/lat=(-116.555,48.8457)
+  // Elevation: 2337m
+  double lon = -116.555;
+  double lat = 48.8457;
+
+  // n48_118: Highest point = 2219m @ lon/lat=(-117.46,48.9285)
+  // Elevation: 2205m
+  double lon_118 = -117.46;
+  double lat_118 = 48.9285;
+
+  // n48_119: Highest point = 2172m @ lon/lat=(-118.466,48.7026)
+  //double lon = -118.466;
+  //double lat = 48.7026;
+
+  query_database(lon, lat);
   return 0;
 }
