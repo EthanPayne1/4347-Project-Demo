3699fcbfe25a3b061f1c3dba9ee6fe9599070d3c
EthanPayne1 <payne.ethan6@yahoo.com>
2025-02-20 10:35:12 -0600

    fixed url

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ DB_DUMP_LINK="https://drive.google.com/file/d/1oTv9i2GeOrIwMhjSwicV4rWL71nf3PCk/
 # Be aware that a level 2 DTED map of the US is HUGE. 
 # The compressed folder(which your downloading) is around 8gb and the uncompressed version is just under 30gb
 # Loading this much data into a program might blow up your PC
-ENTIRE_US_LINK="https://drive.google.com/file/d/1oTv9i2GeOrIwMhjSwicV4rWL71nf3PCk/view?usp=sharing"
+ENTIRE_US_LINK="https://drive.google.com/file/d/1-IXr8wSxu1lgKkocce8zpUrvrJMyaMG_/view?usp=sharing"
 echo "Downloading the entire US."
 
 curl -L -o $ENTIRE_US_LINK
